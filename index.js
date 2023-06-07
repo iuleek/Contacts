fe2322f3b9bebfbf5a2b5249db958c4a89800089
Iuliana Gugeanu <iugu0602@createq.space>
2023-06-07 18:02:23 +0300

    Code review, added coments, completed js.

--- index.js
+++ index.js
@@ -1,25 +1,57 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://jsonplaceholder.typicode.com/users', true);
-    xhr.onreadystatechange = function() {
-        //readystate : 0-unsent 1-opened 2-headers_received 3-loading 4-done
-        //UNSENT: 0 OPENED: 0 LOADING: 200 DONE: 200
-        if (xhr.readyState === 4 && xhr.status === 200) {
-        var response = JSON.parse(xhr.responseText);
-        var contactsBody = document.getElementById('contactsBody'); //Getting the tbody by id
-        var rows = contactsBody.getElementsByTagName('tr');
-        for (var i = 0; i < 9; i++) { //I only created 9 rows
-          var rowData = response[i];
-          var row = rows[i];
-  
-          var cells = row.getElementsByTagName('td');
-          cells[0].textContent = rowData.name;
-          cells[0].classList.add("contacts__name");
-          cells[1].textContent = rowData.email.toLowerCase();
-          cells[1].classList.add("contacts__email");
-        }
+const check = document.createElement("INPUT");
+check.setAttribute("type", "checkbox");
+check.classList.add("contacts__checkbox")
+
+// Making the switch functional
+document.getElementById("btn-light").addEventListener("click", function () {
+  document.getElementById("btn-dark").style.backgroundColor = "#AE7AFF";
+  document.getElementById("btn-light").style.backgroundColor = "#FAF4F0";
+});
+
+document.getElementById("btn-dark").addEventListener("click", function () {
+  document.getElementById("btn-light").style.backgroundColor = "#AE7AFF";
+  document.getElementById("btn-dark").style.backgroundColor = "#FAF4F0";
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Creating ajax call
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://jsonplaceholder.typicode.com/users', true);
+  xhr.onreadystatechange = function () {
+    // readystate : 0-unsent 1-opened 2-headers_received 3-loading 4-done
+    // UNSENT: 0 OPENED: 0 LOADING: 200 DONE: 200
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var response = JSON.parse(xhr.responseText);
+
+      // Getting the tbody by id contactsBody
+      var contactsBody = document.getElementById('contactsBody');
+
+      // Getting the td by class name 
+      var nameRows = contactsBody.getElementsByClassName('name');
+      var emailRows = contactsBody.getElementsByClassName('email');
+
+      // 9 rows available
+      for (var i = 0; i < 9; i++) {
+        var dataRow = response[i];
+        // Getting the i cell from rowsName
+        var nameCell = nameRows[i];
+        // Getting the i cell from rowsEmail
+        var emailCell = emailRows[i];
+
+        // Clear the text content
+        nameCell.textContent = '';
+        var checkboxContainer = document.createElement('span');
+        // Append the checkbox
+        checkboxContainer.appendChild(check.cloneNode(true));
+        // Append the name text
+        checkboxContainer.appendChild(document.createTextNode(dataRow.name));
+
+        nameCell.appendChild(checkboxContainer);
+        nameCell.classList.add("contacts__name");
+        emailCell.textContent = dataRow.email.toLowerCase();
+        emailCell.classList.add("contacts__email");
       }
-    };
-    xhr.send();
-  });
-  
+    }
+  };
+  xhr.send();
+});
