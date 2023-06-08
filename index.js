9de1fd0d54ca1e72096b52e68ca19808217a6479
Iuliana Gugeanu <iugu0602@createq.space>
2023-06-08 15:42:39 +0300

    Made AJAX call using fetch instead of xhr.

--- index.js
+++ index.js
@@ -14,16 +14,37 @@ document.getElementById("btn-dark").addEventListener("click", function () {
   document.getElementById("btn-dark").style.backgroundColor = "#AE7AFF";
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-  // Creating ajax call
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://jsonplaceholder.typicode.com/users', true);
-  xhr.onreadystatechange = function () {
-    // readystate : 0-unsent 1-opened 2-headers_received 3-loading 4-done
-    // UNSENT: 0 OPENED: 0 LOADING: 200 DONE: 200
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      var response = JSON.parse(xhr.responseText);
+//document.addEventListener('DOMContentLoaded', function () {
+// Creating ajax call
 
+/*******With XHR********/
+// var xhr = new XMLHttpRequest();
+// xhr.open("GET", "https://jsonplaceholder.typicode.com/users", true); // Initializes a request
+// xhr.onreadystatechange = function () {
+// readystate : 0-unsent 1-opened 2-headers_received 3-loading 4-done
+// UNSENT: 0 OPENED: 0 LOADING: 200 DONE: 200
+// if (xhr.readyState === 4 && xhr.status === 200) {
+//   var response = JSON.parse(xhr.responseText);
+// CODE THAT MANIPULATES DATA 
+//   };
+//   xhr.send();
+// });
+
+/*******With FETCH********/
+// Triggers when the initial HTML document has been completely loaded and parsed.
+document.addEventListener('DOMContentLoaded', function () {
+  // Initiate GET request using fetch -> returns a promise
+  fetch("https://jsonplaceholder.typicode.com/users")
+    // Checks if the response was successful (status code 200) using response.ok
+    .then(function (response) {
+      if (response.ok) {
+        // Extract JSON data
+        return response.json();
+      } else {
+        throw new Error("Error:" + response.status);
+      }
+    })
+    .then(function (data) {
       // Getting the tbody by id contactsBody
       var contactsBody = document.getElementById('contactsBody');
 
@@ -33,7 +54,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
       // 9 rows available
       for (var i = 0; i < 9; i++) {
-        var dataRow = response[i];
+        var dataRow = data[i];
         // Getting the i cell from rowsName
         var nameCell = nameRows[i];
         // Getting the i cell from rowsEmail
@@ -52,7 +73,10 @@ document.addEventListener('DOMContentLoaded', function () {
         emailCell.textContent = dataRow.email.toLowerCase();
         emailCell.classList.add("contacts__email");
       }
-    }
-  };
-  xhr.send();
+    })
+    // Catch block executed if there was an error during fetch request or JSON parsing
+    .catch(function (error) {
+      console.error(error);
+    });
 });
+
