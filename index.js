f1c64e2108f5ff474a5d21bc60dc5b7ec8e4a4dd
Iuliana Gugeanu <iugu0602@createq.space>
2023-06-09 12:10:45 +0300

    Made the switch theme to dark feature.

--- index.js
+++ index.js
@@ -3,17 +3,6 @@ const check = document.createElement("INPUT");
 check.setAttribute("type", "checkbox");
 check.classList.add("contacts__checkbox")
 
-// Making the switch functional
-document.getElementById("btn-light").addEventListener("click", function () {
-  document.getElementById("btn-dark").style.backgroundColor = "#FAF4F0";
-  document.getElementById("btn-light").style.backgroundColor = "#AE7AFF";
-});
-
-document.getElementById("btn-dark").addEventListener("click", function () {
-  document.getElementById("btn-light").style.backgroundColor = "#FAF4F0";
-  document.getElementById("btn-dark").style.backgroundColor = "#AE7AFF";
-});
-
 //document.addEventListener('DOMContentLoaded', function () {
 // Creating ajax call
 
@@ -80,3 +69,120 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+// Making the switch functional
+
+/***********************EVENT LISTENER DARK THEME********************/
+document.getElementById("btn-dark").addEventListener("click", function () {
+  document.getElementById("btn-light").style.backgroundColor = "#FAF4F0";
+  document.getElementById("btn-dark").style.backgroundColor = "#AE7AFF";
+
+  /*Changing background*/
+  document.body.classList.add("dark-theme");
+  
+  /*Changing icons in topBar*/
+  var search = document.getElementsByClassName("search");
+  search[0].classList.add("search__dark");
+  
+  var notifications = document.getElementsByClassName("notifications");
+  notifications[0].classList.add("notifications__dark");
+  
+  var apps = document.getElementsByClassName("apps");
+  apps[0].classList.add("apps__dark");
+
+  /*Changing text color of btns and appearance*/
+  const buttons = document.querySelectorAll("button");
+  for(var i = 0; i<buttons.length; i++){
+    buttons[i].classList.add("btn__dark");
+  }
+  var btnViews = document.getElementsByClassName("views__btn");
+  for(var i = 0; i<btnViews.length; i++){
+    btnViews[i].classList.add("views__btn__dark");
+  }
+  var btnOptions = document.getElementsByClassName("opt__btn");
+  for(var i = 0; i<btnOptions.length; i++) {
+    btnOptions[i].classList.add("opt__btn__dark");
+  }
+  btnOptions[0].classList.add("sort__dark");
+  btnOptions[1].classList.add("bulk__dark");
+
+  document.getElementsByClassName("create__btn")[0].classList.remove("btn__dark");
+
+  var btnsPagin = document.getElementsByClassName("pagin__btn");
+    btnsPagin[0].classList.add("btn__prev__dark");
+    btnsPagin[1].classList.add("btn__next__dark");
+  document.getElementsByClassName("links__btn")[0].classList.add("links__btn__dark");
+  /*Changing links color of bottom*/
+  var links = document.getElementsByClassName("link");
+  for(var i = 0; i<links.length; i++)
+  links[i].classList.add("link__dark");
+
+  /*Changing table appereance*/
+  document.getElementById("contactsTable").classList.add("table__dark");
+  const tableRows = document.getElementsByTagName("tr");
+  for(var i = 0; i<tableRows.length; i++) {
+  tableRows[i].classList.add("tr__dark");
+  }
+
+  var theaderElements = document.getElementsByClassName("theader-icon");
+  for(var i = 0; i<theaderElements.length; i++) {
+    theaderElements[i].classList.add("theader-icon__dark");
+    }
+});
+
+/*****************EVENT LISTENER LIGHT THEME*********************/
+document.getElementById("btn-light").addEventListener("click", function () {
+  document.getElementById("btn-dark").style.backgroundColor = "#FAF4F0";
+  document.getElementById("btn-light").style.backgroundColor = "#AE7AFF";
+
+   /*Changing background*/
+   document.body.classList.remove("dark-theme");
+  
+   /*Changing icons in topBar*/
+   var search = document.getElementsByClassName("search");
+   search[0].classList.remove("search__dark");
+   
+   var notifications = document.getElementsByClassName("notifications");
+   notifications[0].classList.remove("notifications__dark");
+   
+   var apps = document.getElementsByClassName("apps");
+   apps[0].classList.remove("apps__dark");
+ 
+   /*Changing text color of btns and appearance*/
+   const buttons = document.querySelectorAll("button");
+   for(var i = 0; i<buttons.length; i++){
+     buttons[i].classList.remove("btn__dark");
+   }
+   var btnViews = document.getElementsByClassName("views__btn");
+   for(var i = 0; i<btnViews.length; i++){
+     btnViews[i].classList.remove("views__btn__dark");
+   }
+   var btnOptions = document.getElementsByClassName("opt__btn");
+   for(var i = 0; i<btnOptions.length; i++) {
+     btnOptions[i].classList.remove("opt__btn__dark");
+   }
+   btnOptions[0].classList.remove("sort__dark");
+   btnOptions[1].classList.remove("bulk__dark");
+ 
+   document.getElementsByClassName("create__btn")[0].classList.remove("btn__dark");
+ 
+   var btnsPagin = document.getElementsByClassName("pagin__btn");
+     btnsPagin[0].classList.remove("btn__prev__dark");
+     btnsPagin[1].classList.remove("btn__next__dark");
+   document.getElementsByClassName("links__btn")[0].classList.remove("links__btn__dark");
+   /*Changing links color of bottom*/
+   var links = document.getElementsByClassName("link");
+   for(var i = 0; i<links.length; i++)
+   links[i].classList.remove("link__dark");
+ 
+   /*Changing table appereance*/
+   document.getElementById("contactsTable").classList.remove("table__dark");
+   const tableRows = document.getElementsByTagName("tr");
+   for(var i = 0; i<tableRows.length; i++) {
+   tableRows[i].classList.remove("tr__dark");
+   }
+   var theaderElements = document.getElementsByClassName("theader-icon");
+  for(var i = 0; i<theaderElements.length; i++) {
+    theaderElements[i].classList.remove("theader-icon__dark");
+    }
+ 
+});
